5b041e42267c47513c2ac8d910a0e9eb62c12825
may-ank0813 <mayanksaini1913@gmail.com>
2022-12-14 00:05:17 +0530

    setting changes

--- yoga/settings.py
+++ yoga/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#_-%+v7)a&q$viuv+1n0^e6#+ybc$zq-)3ug&efuv0k3qf$cs('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
