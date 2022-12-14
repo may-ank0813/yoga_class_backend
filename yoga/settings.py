071d1036f7183e3bbfbea17b3b6a294c004fc150
may-ank0813 <mayanksaini1913@gmail.com>
2022-12-14 22:24:37 +0530

    settings changed

--- yoga/settings.py
+++ yoga/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-#_-%+v7)a&q$viuv+1n0^e6#+ybc$zq-)3ug&efuv0k3qf$cs(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -129,8 +129,8 @@ AUTH_USER_MODEL = "user.CustomUser"
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
